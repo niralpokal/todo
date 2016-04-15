8694ca0129beee38dd299c9d36822a2baf473b05
Niral Pokal <niralpokal@gmail.com>
2016-04-15 13:10:27 -0400

    I moved this above the http.get call.

--- public/app.js
+++ public/app.js
@@ -5,11 +5,11 @@ app.controller('homeController', home);
 app.$inject = ['$http'];
 
 function home($http){
+  var vm = this
   var user = $http.get('http://localhost:8080/user');
   user.then(function(info){
     vm.message = info.data.name
   })
-  var vm = this
 }
 
 
