52e1ef4298df5f13e4a924795d2f050d24fea4f0
Niral Pokal <niralpokal@gmail.com>
2016-04-18 13:01:23 -0700

    I completed exercise 118.

--- app.js
+++ app.js
@@ -1,23 +1,65 @@
 var app = require('express')();
 var express = require('express')
+var MongoClient = require('mongodb').MongoClient;
+var jsonParser = require('body-parser').json();
+var url = "mongodb://localhost:27017/test"
 
 app.use(express.static('./public/'))
 
+
 app.get('/user', function(req, res) {
-  var myData ={
-    name: 'Niral',
-    location: 'Newport Beach, Ca'
-  }
-  res.json(myData);
+   var myData ={
+       name: 'Niral',
+       location: 'Newport Beach, Ca'
+     }
+     res.json(myData);
 });
 
-app.get('/todos/:users', function(req, res) {
-  if(req.params.users === 'Niral'){
-    var todos = ['Code', 'Dinner'];
-    res.send(todos)
-  }else{
-    res.sendStatus(404)
-  }
+app.post('/list', jsonParser,  function(req, res) {
+  var list = req.body
+  MongoClient.connect(url, function(err,db){
+    if(err){throw err}
+    else{
+      db.collection('lists').insert([list], function(err, results){
+        db.close();
+        res.sendStatus(200);
+      });
+    }
+  })
 });
 
-app.listen(8080);
+app.get('/list/:users', function(req, res) {
+  var user = req.params.users
+  MongoClient.connect(url, function(err, db){
+    db.collection('lists').find({'user':user}).toArray(function(err, docs){
+      if(err){throw err}
+      else{
+        db.close();
+        res.send(docs[0].list);
+      }
+    })
+  })
+});
+
+/*app.put('/list',jsonParser, function(req, res){
+
+})*/
+
+app.delete('/list/:users', function(req,res){
+  var user = req.params.users;
+  MongoClient.connect(url, function(err, db){
+    db.collection('lists').remove({'user':user}, function(err, results){
+      if (err){ throw err}
+      else{
+        db.close();
+        res.sendStatus(200);
+      }
+    })
+  })
+});
+
+if(!require.main.loaded){
+  var server = app.listen(8080)
+}
+
+module.exports = app
--- public/app.js
+++ public/app.js
@@ -8,19 +8,28 @@ function home($http){
   var vm = this
   var user = $http.get('http://localhost:8080/user');
   user.then(function(info){
-    vm.message = info.data.name
+    vm.message = "Welcome Home, "
+    vm.user = info.data
   })
 }
 
+app.directive('greeting', greeting);
+
+function greeting(){
+  return{
+    templateUrl:'home/greeting.directive.html'
+  }
+}
 
 app.controller('todoController', todo);
 
 app.$inject = ['$http'];
+app.$inject = ['$scope'];
 
-function todo($http){
+function todo($http, $scope){
   var vm = this;
-  var todos = $http.get('http://localhost:8080/todos/Niral')
+  var todos = $http.get('http://localhost:8080/list/Niral')
   todos.then(function(info){
-    vm.todo = info.data
+    $scope.list = info.data
   })
 }
