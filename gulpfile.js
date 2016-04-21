8828fa4ebf759fff9448e64887ea4e2177b58ccc
Niral Pokal <niralpokal@gmail.com>
2016-04-21 16:09:03 -0700

    I updated my gulpfile to pass the travis test.

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var nodemon = require('gulp-nodemon')
 var mocha = require('gulp-mocha')
 var casperJs = require('gulp-casperjs')
+var app = require('./app.js')
 
 gulp.task('test', ['casper'],  function(){
     return gulp.src('app.spec.js').pipe(mocha()).once('end', function(){
@@ -10,5 +11,7 @@ gulp.task('test', ['casper'],  function(){
 });
 
 gulp.task('casper', function(){
-  return gulp.src('casper.spec.js').pipe(casperJs());
+    var port = 8080
+    var server = app.listen(port);
+    return gulp.src('casper.spec.js').pipe(casperJs());
 })
